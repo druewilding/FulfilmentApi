ab522cb11c96ddf7015e7efac1c503f1fe24a45a
Drue Wilding <drue@wilding.dk>
2026-05-09 09:25:22 +0200

    fix: Make strings required in the address object

--- src/FulfilmentApi.Domain/Address.cs
+++ src/FulfilmentApi.Domain/Address.cs
@@ -2,7 +2,21 @@ namespace FulfilmentApi.Domain;
 
 public record Address
 {
-    public required string Street { get; init; }
-    public required string PostalCode { get; init; }
-    public required string City { get; init; }
+    public string Street { get; init; }
+    public string PostalCode { get; init; }
+    public string City { get; init; }
+
+    public Address(string street, string postalCode, string city)
+    {
+        if (string.IsNullOrWhiteSpace(street))
+            throw new ArgumentException("Street cannot be empty.", nameof(street));
+        if (string.IsNullOrWhiteSpace(postalCode))
+            throw new ArgumentException("PostalCode cannot be empty.", nameof(postalCode));
+        if (string.IsNullOrWhiteSpace(city))
+            throw new ArgumentException("City cannot be empty.", nameof(city));
+
+        Street = street;
+        PostalCode = postalCode;
+        City = city;
+    }
 };
